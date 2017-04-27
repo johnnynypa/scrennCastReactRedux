fc9299edc61849289f16f2c08724b0978009ab2d
Johnny Pacheco <johnnye96pv@gmail.com>
2017-04-26 23:32:07 -0500

    Video 8

--- client/components/signup/SignupForm.js
+++ client/components/signup/SignupForm.js
@@ -1,107 +1,122 @@
 import React from 'react';
-import timezones from '../../data/timezone';
+import timezones from '../../data/timezones';
 import map from 'lodash/map';
+import classnames from 'classnames';
 
-class SignupForm extends React.Component{
+class SignupForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: '',
+      email: '',
+      password: '',
+      passwordConfirmation: '',
+      timezone: '',
+	  errors: {},
+	  isLoading: false
+    }
 
-	constructor(props){
-		super(props);
-		this.state = {
-			username: "",
-			email: "",
-			password: "",
-			passwordConfirmation: "",
-			timezone: ""
-		};
+    this.onChange = this.onChange.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
+  }
 
-		this.onChange = this.onChange.bind(this);
-		this.onSubmit = this.onSubmit.bind(this);
-	}
-	onChange(e){
-		this.setState({
-			[e.target.name]: e.target.value
-		})
-	}
-	onSubmit(e){
-		e.preventDefault();
-		this.props.userSignupRequest(this.state);
-	}
-	render(){
-		const options = map(timezones, (val, key) =>
-			<option key={val} value={val} >{key}</option>
-		);
-		return (
-			<form onSubmit={this.onSubmit} >
-				<h1>Join our community</h1>
-				<div className="form-group">
-					<label className="control-label">UserName</label>
-					<input 
-						value = {this.state.username}
-						onChange= {this.onChange}
-						type="text"
-						className="form-control"
-						name="username"
-						required
-					/>
-				</div>
-				<div className="form-group">
-					<label className="control-label">Email</label>
-					<input 
-						value = {this.state.email}
-						onChange= {this.onChange}
-						type="mail"
-						className="form-control"
-						name="email"
-						required
-					/>
-				</div>
-				<div className="form-group">
-					<label className="control-label">Password</label>
-					<input 
-						value = {this.state.password}
-						onChange= {this.onChange}
-						type="password"
-						className="form-control"
-						name="password"
-						required
-					/>
-				</div>
-				<div className="form-group">
-					<label className="control-label">Password Confirmation</label>
-					<input 
-						value = {this.state.passwordConfirmation}
-						onChange= {this.onChange}
-						type="password"
-						className="form-control"
-						name="passwordConfirmation"
-						required
-					/>
-				</div>
-				<div className="form-group">
-					<label htmlFor="timezone" className="control-label">Time Zone</label>
-					<select
-						className="form-control"
-						name="timezone"
-						id="timezone"
-						value= {this.state.timezone}
-						onChange= {this.onChange}
-					>
-						<option value="" disabled >Chosse your timezone</option>
-						{options}
-					</select>
-				</div>
-				<div className="form-group">
-					<button className="btn btn-primary btn-lg">
-						Sign Up
-					</button>
-				</div>
-			</form>
-		);
-	}
+  onChange(e) {
+    this.setState({ [e.target.name]: e.target.value });
+  }
+
+  onSubmit(e){
+	  //vaciamos el estado que tiene los errores para su actualizacion y mostramos el cargando
+	this.setState({errors: {}, isLoading:true});
+    e.preventDefault();
+    this.props.userSignupRequest(this.state).then(
+		() => {},
+		({data}) => this.setState({errors: data, isLoading:false})
+	)
+  }
+
+  render() {
+    const options = map(timezones, (val, key) =>
+      <option key={val} value={val}>{key}</option>
+    );
+	const {errors} = this.state;
+    return (
+      <form onSubmit={this.onSubmit}>
+        <h1>Join our community!</h1>
+
+        <div className= {classnames("form-group", {'has-error': errors.username} ) }>
+          <label className="control-label">Username</label>
+          <input
+            value={this.state.username}
+            onChange={this.onChange}
+            type="text"
+            name="username"
+            className="form-control"
+          />
+		  {errors.username && <span className="help-block" > {errors.username} </span> }
+        </div>
+
+        <div className= {classnames("form-group", {'has-error': errors.email} ) }>
+          <label className="control-label">Email</label>
+          <input
+            onChange={this.onChange}
+            value={this.state.email}
+            type="text"
+            name="email"
+            className="form-control"
+          />
+		  {errors.email && <span className="help-block" > {errors.email} </span> }
+        </div>
+
+        <div className= {classnames("form-group", {'has-error': errors.password} ) }>
+          <label className="control-label">Password</label>
+          <input
+            onChange={this.onChange}
+            value={this.state.password}
+            type="password"
+            name="password"
+            className="form-control"
+          />
+		  {errors.password && <span className="help-block" > {errors.password} </span> }
+        </div>
+
+        <div className= {classnames("form-group", {'has-error': errors.passwordConfirmation} ) }>
+          <label className="control-label">Password Confirmation</label>
+          <input
+            onChange={this.onChange}
+            value={this.state.passwordConfirmation}
+            type="password"
+            name="passwordConfirmation"
+            className="form-control"
+          />
+		  {errors.passwordConfirmation && <span className="help-block" > {errors.passwordConfirmation} </span> }
+        </div>
+
+        <div className= {classnames("form-group", {'has-error': errors.timezone} ) }>
+          <label className="control-label">Timezone</label>
+          <select
+            className="form-control"
+            name="timezone"
+            onChange={this.onChange}
+            value={this.state.timezone}
+          >
+            <option value="" disabled>Choose Your Timezone</option>
+            {options}
+          </select>
+		  {errors.timezone && <span className="help-block" > {errors.timezone} </span> }
+        </div>
+
+        <div className="form-group">
+          <button disabled={this.state.isLoading} className="btn btn-primary btn-lg">
+            Sign up
+          </button>
+        </div>
+      </form>
+    );
+  }
 }
 
-SignupForm.prototype = {
-	userSignupRequest: React.PropTypes.func.isRequired
+SignupForm.propTypes = {
+  userSignupRequest: React.PropTypes.func.isRequired
 }
 
 export default SignupForm;
--- client/components/signup/SignupPage.js
+++ client/components/signup/SignupPage.js
@@ -1,27 +1,24 @@
 import React from 'react';
-import SignupForm from '../signup/SignupForm'
-import {connect} from 'react-redux'
-import {userSignupRequest} from '../../actions/signupActions';
+import SignupForm from './SignupForm';
+import { connect } from 'react-redux';
+import { userSignupRequest } from '../../actions/signupActions';
 
 class SignupPage extends React.Component {
   render() {
-    const {userSignupRequest} = this.props;
+    const { userSignupRequest } = this.props;
     return (
-      <div>
-        <h1>Sign up page</h1>
-        <div className="row">
-          <div className="col-md-4 col-md-offset-4">
-            <SignupForm userSignupRequest={userSignupRequest} />
-          </div>
+      <div className="row">
+        <div className="col-md-4 col-md-offset-4">
+          <SignupForm userSignupRequest={userSignupRequest} />
         </div>
       </div>
     );
   }
 }
 
-SignupPage.prototype = {
-	userSignupRequest: React.PropTypes.func.isRequired
+SignupPage.propTypes = {
+  userSignupRequest: React.PropTypes.func.isRequired
 }
 
-export default connect(null, {userSignupRequest} )(SignupPage);
-//export default connect((state) =>{ return  }, {userSignupRequest} )(SignupPage);
+
+export default connect(null, { userSignupRequest })(SignupPage);
